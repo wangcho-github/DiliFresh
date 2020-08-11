3427cbb9d45a0040d39a97bd0931f5afa4a88484
wangcho-github <2523839672@qq.com>
2020-08-11 11:34:42 +0800

    首次提交版本

---
+++ src/main/java/com/web/DiliFresh/DiliFreshApplication.java
@@ -0,0 +1,13 @@
+package com.web.DiliFresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiliFreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiliFreshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/DiliFresh/DiliFreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.DiliFresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiliFreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
