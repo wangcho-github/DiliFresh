7b16e05da0f31bb2a3d6a47c01536acdad327847
fany_f <admin@DESKTOP-KIJHIS4>
2020-08-13 14:13:28 +0800

    fany_f0813

--- src/main/java/com/web/DiliFresh/DiliFreshApplication.java
+++ src/main/java/com/web/DiliFresh/DiliFreshApplication.java
@@ -2,8 +2,12 @@ package com.web.DiliFresh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@ComponentScan("com.web.DiliFresh")
 public class DiliFreshApplication {
 
 	public static void main(String[] args) {
